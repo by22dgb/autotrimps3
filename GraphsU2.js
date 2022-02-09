d7f8297559660f878e24f964d78a353de6696058
by22dgb <by22dgb@gmail.com>
2022-02-09 16:07:36 +0000

    本地化

--- GraphsU2.js
+++ GraphsU2.js
@@ -1,4 +1,4 @@
-var basepath = 'https://zorn192.github.io/AutoTrimps/';
+var basepath = 'https://likexia.gitee.io/autotrimps3/';
 var allSaveData2 = [];
 var graph2Data2 = [];
 var tmpGraphData2 = JSON.parse(localStorage.getItem('allSaveData2'));
